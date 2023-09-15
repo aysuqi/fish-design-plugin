f487d86ba7f1a4ac4a3174655f13235206f95a5d
yusunqi <ysuqi01@126.com>
2023-09-16 01:27:19 +0800

    feat: 第一次提交

---
+++ packages/utils/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Description:
+ * @Author: yusunqi
+ * @Date: 2023-09-15 22:54:58
+ * @LastEditTime: 2023-09-16 01:04:29
+ * Copyright (c) 2023 by yusunqi, All Rights Reserved.
+ */
+const a = 10
+console.log('a :>> ', a)
